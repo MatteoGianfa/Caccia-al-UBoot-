adb58ffd863de49a89eb98d2ef7ff034bbf02871
MarcoZandona4infb <131490208+MarcoZandona4infb@users.noreply.github.com>
2023-04-29 18:18:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,18 +11,35 @@ function gioco(){
   window.location.href = "Gioco.htm";
 }
 function nave1(){
-  gameplay(1);
+  gameplay();
+  setTimeout(2000);
+  inizia(1);
 }
 function nave2(){
-  gameplay(2);
+  gameplay();
+  setTimeout(2000);
+  inizia(2);
 }
 function nave3(){
-  gameplay(3);
+  gameplay();
+  setTimeout(2000);
+  inizia(3);
 }
 function nave4(){
-  gameplay(4);
+  gameplay();
+  setTimeout(2000);
+  inizia(4);
 }
 function gameplay(n){
   window.location.href = "gameplay.htm";
-  console.log("ciao");
+}
+function inizia(){
+  let t;
+  for(let i = 0; i < 40; i++) {
+    for(let j = 0; j< 40; j++) {
+        t+=`<div>${i+1}</div>`;
+    }
+    t+="<br>";
+  }
+  document.getElementById("campo").innerHTML="ciao";
 }
