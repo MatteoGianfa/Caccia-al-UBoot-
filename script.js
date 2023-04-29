bc0b1c83df2fdb6a49b3992b973b735126cc10c3
MarcoZandona4infb <131490208+MarcoZandona4infb@users.noreply.github.com>
2023-04-29 17:21:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 class nave{
-  #
+  #n
+  constructor(n){
+    this.#n=n;
+  }
+  get n(){
+    return this.#n;
+  }
 }
 function gioco(){
   window.location.href = "Gioco.htm";
@@ -17,5 +23,15 @@ function nave4(){
   inizia(4);
 }
 function inizia(n){
-  
+  for(var i = 0; i < 40; i++) {
+    for(var j = 0; j< 40; j++) {
+        var div = document.createElement("div");
+        div.style.width = "25px";
+        div.style.height = "25px";
+        div.style.background = "red";
+        var jump = document.createElement("br");
+        document.getElementById("container").appendChild(jump);
+        document.getElementById("container").appendChild(div);
+    }
+  }
 }
