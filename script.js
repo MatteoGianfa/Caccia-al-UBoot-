a4ce2df9906724a2dc43cbac1fc29585217714ff
MarcoZandona4infb <131490208+MarcoZandona4infb@users.noreply.github.com>
2023-04-29 18:41:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -26,13 +26,15 @@ function gameplay(n){
   window.location.href = "gameplay.htm";
 }
 function inizia(){
-  console.log("ciao");
-  let t;
+  let t="";
   for(let i = 0; i < 40; i++) {
     for(let j = 0; j< 40; j++) {
-        t+=`<div>${i+1}</div>`;
+        t+=`<div class="casella" id="pos${i}${j}">${i+1}</div>`;
     }
     t+="<br>";
   }
-  document.getElementById("campo").innerHTML="ciao";
+  document.getElementById("campo").innerHTML=t;
+}
+if (document.URL == "file:///C:/Users/admin/Downloads/Caccia-al-UBoot--main/Caccia-al-UBoot--main/gameplay.htm"){
+  document.addEventListener("DOMContentLoaded", inizia, false);
 }
