d930e9ba76a4bb68feb810cf46577ab434a34f23
MarcoZandona4infb <131490208+MarcoZandona4infb@users.noreply.github.com>
2023-04-29 17:03:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function gioco(){
   window.location.href = "Gioco.htm";
 }
+function nave1(){
+  inizia(1);
+}
+function nave2(){
+  inizia(2);
+}
+function nave3(){
+  inizia(3);
+}
+function nave4(){
+  inizia(4);
+}
+function inizia(let n){
+  
+}
